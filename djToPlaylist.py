06a1bcacfce84829fa15e093047ec5b929642b51
Isaac Staugaard <isaacestaugaard@gmail.com>
2017-11-04 11:05:15 -0700

    Refactored code and fixed minor bugs

--- djToPlaylist.py
+++ djToPlaylist.py
@@ -51,7 +51,19 @@ def init_driver():
 	driver.wait = WebDriverWait(driver, 5)
 	return driver
 
+
+
 def lookup(driver, query):
+	venueAndDate = tracklistsSite(driver,query)
+	time.sleep(3)
+	linkDescription = getLink(driver,query)
+	time.sleep(3)
+	getDataAndMakePlaylist(driver, venueAndDate, linkDescription, query)
+	time.sleep(4)
+
+
+
+def tracklistsSite(driver,query):
 	driver.get("https://www.1001tracklists.com/")
 	try:
 		#Find the search menu and change the value from tracklists to Djs
@@ -66,112 +78,153 @@ def lookup(driver, query):
 		button = driver.wait.until(EC.element_to_be_clickable(
 			(By.ID, "searchBtn")))
 		box.send_keys(query)
+
 		try:
 			button.click()
 		except ElementNotVisibleException:
 			button = driver.wait.until(EC.visibility_of_element_located(
 				(By.NAME, "btnG")))
 			button.click()
+
 		#Click on the artists link to go to his page
 		try: 
 			driver.find_element_by_xpath("//*[@id='middleTbl']/tbody/tr/td/table/tbody/tr[2]/td/div[1]/a").click()
 		except:
 			print("Artist not found")
-		time.sleep(5)
+		time.sleep(3)
+
 		#Click on the 1st menu item (usually closes the Most Liked 2017 Tab)
-		driver.find_element_by_xpath("//*[@id='leftContent']/div[2]/div[1]/a[1]").click()
+		side = driver.find_elements_by_xpath("//*[@id='leftContent']/div")
+		side = side[-1]
+		mlElem = side.find_element_by_xpath("div[1]/a[1]")
+		mlElem.click()
+		time.sleep(1)
+
 		#Click on the second to last menu item (the Most Viewed Tracklists Ever tab)
-		tempElem = driver.find_elements_by_xpath("//*[@id='leftContent']/div[2]/div[1]/a")
-		tempElem[-2].click()
+		mvElem = side.find_elements_by_xpath("div[1]/a")
+		mvElem[-2].click()
+		time.sleep(1)
+
 		#Click on the first link from the Most Viewed Tracklists Ever tab
-		tempElem = driver.find_elements_by_xpath("//*[@id='leftContent']/div[2]/div[1]/div")
-		tempElem = tempElem[-2]
-		tempElem.find_element_by_xpath("table/tbody/tr/td/div/a").click()
+		mvteElem = side.find_elements_by_xpath("div[1]/div")
+		mvteElem = mvteElem[-2]
+		mvteElem.find_element_by_xpath("table/tbody/tr/td/div/a").click()
+		time.sleep(1)
+
 		# Get the date and venue of the set
 		elementChild  = driver.find_element_by_xpath("//*[contains(text(), 'TL date')]")
 		element = elementChild.find_element_by_xpath('../following-sibling::td')
 		date = element.text
 		print("DATE: ", date, "\n")
+
 		element = driver.find_elements_by_class_name('sideTop')[-1]
 		element = element.find_element_by_xpath('tbody/tr/th')
 		venue = element.text
 		print("VENUE: ", venue, "\n")
-		link = ""
-		#Get the link to the set (soundcloud,mixcloud or other)
-		try: 
-			element = driver.find_element_by_xpath("//*[@id='mediaItems']/div/ul/li[.//*[contains(.,'SoundCloud')]]")
-		except: 
-			try: 
-				element = driver.find_element_by_xpath("//*[@id='mediaItems']/div/ul/li[.//*[contains(.,'MixCloud')]]")
-			except: 
-				print("No valid link")
-		element.click()
-		#Change frames and retrieve the link
-		try: 
-			driver.switch_to_frame(driver.find_element_by_class_name('scWidget1'))
-			element = driver.find_element_by_xpath("//*[@id='widget']/div[3]/div/div/div[2]/div[1]/div/div[2]/div[1]/div/a")
-			link = element.get_attribute('href')
-			print("LINK: ", link, "\n")
-		except:
-			try: 
-				driver.switch_to_frame(driver.find_element_by_class_name('mcWidget1'))
-				element = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/div[1]/a")
-				link = element.get_attribute('href')
-				print("LINK: ", link, "\n")
-			except:
-				print("Could not change frames and retrieve link")
-		time.sleep(3)
-		artists = []
-		songs = []
-		playlistName = "Songs from " + query + "'s set at " + venue + ' on ' + date 
-		#Creates a new playlist
-		youtube = get_authenticated_service()
 
-		plName = playlists_list_mine(youtube, part='snippet,contentDetails', mine=True, maxResults=25, onBehalfOfContentOwner='', onBehalfOfContentOwnerChannel='')
-		for elem1 in plName:
-			if(playlistName == elem1):
-				print("There is already a playlist with this name. Exiting \n")
-				return
+		venuedate = []
+		venuedate.append(venue)
+		venuedate.append(date)
+		return venuedate
 
-		try:
-			playlistID = add_playlist(youtube, playlistName, args)
-		except urllib.error.HTTPError as e:
-			print ('An HTTP error %d occurred:\n%s' % (e.resp.status, e.content))
-		driver.switch_to.default_content()
-		#Loop through the songs from the set --> add song/artist to respective list --> youtube API to search for videoID --> add video to playlist
-		for elem in driver.find_elements_by_xpath("//*[@itemprop='tracks']"):
-			elemList = elem.find_elements_by_tag_name("meta")
-			toSplit = elemList[0].get_attribute("content")
-			splitList = toSplit.split(" - ")
-			query = splitList[0] + ' ' + splitList[1]
-			artists.append(splitList[0])
-			songs.append(splitList[1])
-			#Search on youtube for the video
-			videoID = youtube_search(query, 10)
-			#If there is no video for the song, skip it
-			if not videoID:
-				continue
-			#Appends item to the playlist
-			playlist_items_insert(youtube,{'snippet.playlistId': playlistID,'snippet.resourceId.kind': 'youtube#video','snippet.resourceId.videoId': videoID[0] ,'snippet.position': ''}, part='snippet',onBehalfOfContentOwner='')
-		sizeOfPlaylist = len(artists)
-		time.sleep(5)
 	except TimeoutException:
 		print("Could not process your request")
+
+
+
+def getLink(driver,query):
+	#Get the link to the set (soundcloud,mixcloud or other)
+	link = "No soundcloud or mixcloud link"
+	try: 
+		element = driver.find_element_by_xpath("//*[@id='mediaItems']/div/ul/li[.//*[contains(.,'SoundCloud')]]")
+	except: 
+		try: 
+			element = driver.find_element_by_xpath("//*[@id='mediaItems']/div/ul/li[.//*[contains(.,'MixCloud')]]")
+		except: 
+			print("No valid link")
+	element.click()
+	time.sleep(2)
+
+	#Change frames and retrieve the link
+	try: 
+		driver.switch_to_frame(driver.find_element_by_class_name('scWidget1'))
+		time.sleep(2)
+		element = driver.find_element_by_xpath("//*[@id='widget']/div[3]/div/div/div[2]/div[1]/div/div[2]/div[1]/div/a")
+		link = element.get_attribute('href')
+		print("LINK: ", link, "\n")
+		return link
+	except:
+		try: 
+			driver.switch_to_frame(driver.find_element_by_class_name('mcWidget1'))
+			time.sleep(2)
+			element = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/div[1]/a")
+			link = element.get_attribute('href')
+			print("LINK: ", link, "\n")
+			return link
+		except:
+			print("Could not change frames and retrieve link")
+	return link
+
+
+
+def getDataAndMakePlaylist(driver, venuedate, link, query):
+	artists = []
+	songs = []
+	playlistName = "Songs from " + query + "'s set at " + venuedate[0] + ' on ' + venuedate[1] 
+
+	#Creates a new playlist
+	youtube = get_authenticated_service()
+	plName = playlists_list_mine(youtube, part='snippet,contentDetails', mine=True, maxResults=25, onBehalfOfContentOwner='', onBehalfOfContentOwnerChannel='')
+	for elem1 in plName:
+		if(playlistName == elem1):
+			print("There is already a playlist with this name. Exiting \n")
+			return
+
+	try:
+		playlistID = add_playlist(youtube, playlistName, link, args)
+	except urllib.error.HTTPError as e:
+		print ('An HTTP error %d occurred:\n%s' % (e.resp.status, e.content))
+	driver.switch_to.default_content()
+
+	#Loop through the songs from the set --> add song/artist to respective list --> youtube API to search for videoID --> add video to playlist
+	for elem in driver.find_elements_by_xpath("//*[@itemprop='tracks']"):
+		elemList = elem.find_elements_by_tag_name("meta")
+		toSplit = elemList[0].get_attribute("content")
+		splitList = toSplit.split(" - ")
+		query = splitList[0] + ' ' + splitList[1]
+		artists.append(splitList[0])
+		songs.append(splitList[1])
+		#Search on youtube for the video
+		videoID = youtube_search(query, 10)
+		#If there is no video for the song, skip it
+		if not videoID:
+			continue
+		#Appends item to the playlist
+		playlist_items_insert(youtube,{'snippet.playlistId': playlistID,'snippet.resourceId.kind': 'youtube#video','snippet.resourceId.videoId': videoID[0] ,'snippet.position': ''}, part='snippet',onBehalfOfContentOwner='')
+	sizeOfPlaylist = len(artists)
+
+
 
 def get_authenticated_service():
   flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
   credentials = flow.run_console()
   return build(API_SERVICE_NAME, API_VERSION, credentials = credentials)
 
-def add_playlist(youtube, playlistName, args):
-	body = dict(snippet=dict(title=playlistName, description=args.description),status=dict(privacyStatus='private'))
+
+
+def add_playlist(youtube, playlistName, link, args):
+	body = dict(snippet=dict(title=playlistName, description=link),status=dict(privacyStatus='private'))
 	#body = dict(snippet=dict(title=args.DJ, description=args.description),status=dict(privacyStatus='private')) 
 	playlists_insert_response = youtube.playlists().insert(part='snippet,status',body=body).execute()
 	print ('New playlist ID: %s' % playlists_insert_response['id'])
 	return playlists_insert_response['id']
 
+
+
 def print_response(response):
 	print(response)	
+
+
 
 # Build a resource based on a list of properties given as key-value pairs.
 # Leave properties with empty values out of the inserted resource.
@@ -212,6 +265,8 @@ def build_resource(properties):
         ref = ref[key]
   return resource
 
+
+
 # Remove keyword arguments that are not set
 def remove_empty_kwargs(**kwargs):
   good_kwargs = {}
@@ -221,6 +276,8 @@ def remove_empty_kwargs(**kwargs):
         good_kwargs[key] = value
   return good_kwargs
 
+
+
 def playlist_items_insert(client, properties, **kwargs):
   resource = build_resource(properties)
   kwargs = remove_empty_kwargs(**kwargs)
@@ -229,6 +286,8 @@ def playlist_items_insert(client, properties, **kwargs):
     **kwargs
   ).execute()
   #return print_response(response)
+
+
 
 def youtube_search(query, maxNum):
   youtube = build(API_SERVICE_NAME, API_VERSION,
@@ -254,6 +313,8 @@ def youtube_search(query, maxNum):
   #print ('Videos:\n', videos[0])
   return (videos)
 
+
+
  # Sample python code for playlists.list
 def playlists_list_mine(client, **kwargs):
   # See full sample for function
@@ -268,12 +329,13 @@ def playlists_list_mine(client, **kwargs):
   #return print_response(response)
 
 
+
 if __name__=="__main__":
 	argparser = argparse.ArgumentParser()
 	argparser.add_argument("DJ",help = 'DJ Name')
-	argparser.add_argument('--description',
-	  default='All the songs from the setlist',
-	  help='The description of the new playlist.')
+	#argparser.add_argument('--description',
+	#  default='All the songs from the setlist',
+	#  help='The description of the new playlist.')
 	driver = init_driver()
 	args = argparser.parse_args()
 	DJ = args.DJ
